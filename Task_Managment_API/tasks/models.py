013e95903e3885390abb19f3be9ce7771f31c63a
kalid-2024 <kalidmohammed236@gmail.com>
2025-10-05 21:43:01 +0300

    Add  Initial Task model with migrations

--- Task_Managment_API/tasks/models.py
+++ Task_Managment_API/tasks/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Task(models.Model):
+    owner = models.ForeignKey(User, related_name='tasks', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateTimeField(auto_now=True)
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self) -> str:
+        return f"{self.title} - {'Completed' if self.completed else 'Pending'}"
