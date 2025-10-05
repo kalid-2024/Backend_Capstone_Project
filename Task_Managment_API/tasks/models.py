7821b936a17411bf2ea4c7b1c848472977522fd2
kalid-2024 <kalidmohammed236@gmail.com>
2025-10-05 23:13:51 +0300

    correction in the model

--- Task_Managment_API/tasks/models.py
+++ Task_Managment_API/tasks/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from
+from django.conf import settings
 
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
