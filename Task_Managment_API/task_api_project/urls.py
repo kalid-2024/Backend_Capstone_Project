6eb2d3d4dd20239e8d485fec52c150b4e06c10c2
khalid_mohammed <kalidmohammed236@gmail.com>
2025-10-12 20:17:30 +0300

    Fix URL configuration in project and tasks app

--- Task_Managment_API/task_api_project/urls.py
+++ Task_Managment_API/task_api_project/urls.py
@@ -16,22 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
-from tasks.views import UserViewSet, TaskViewSet
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
 
-
-router = routers.DefaultRouter()
-router.register('users', UserViewSet)
-router.register('tasks', TaskViewSet)  
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
-    # JWT auth endpoints
-path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path('', include(tasks.urls)),
+
 ]
---
+++ Task_Managment_API/tasks/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path, include
+from rest_framework import routers  
+from tasks.views import UserViewSet, TaskViewSet
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'tasks', TaskViewSet)
+
+urlpatterns = [
+    path('api/', include(router.urls)),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
