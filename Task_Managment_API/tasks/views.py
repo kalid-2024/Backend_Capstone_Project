5e5ac5fe06968da8f1a19e3f99d98cbd298f2909
khalid_mohammed <kalidmohammed236@gmail.com>
2025-10-18 22:15:57 +0300

    Implement user register,login,profile function based views in the userviewset and task completion endpoints with queryset fixes

--- Task_Managment_API/tasks/views.py
+++ Task_Managment_API/tasks/views.py
@@ -6,7 +6,7 @@ from .models import Task
 from .serializers import TaskSerializer, UserSerializer
 from django.contrib.auth import get_user_model
 from rest_framework.permissions import IsAuthenticated, AllowAny
-from .permissions import IsOwner,IsAdminOrSelf
+from .permissions import IsOwner, IsAdminOrSelf
 from rest_framework.decorators import action
 from django_filters.rest_framework import DjangoFilterBackend
 
@@ -21,9 +21,32 @@ class UserViewSet(viewsets.ModelViewSet):
     ordering_fields = ["username", "email"]
 
     def get_permissions(self):
-        if self.action in ["create"]:
+        if self.action in ["create", "register"]:
             return [AllowAny()]
         return [IsAdminOrSelf()]
+
+    @action(detail=False, methods=['post'], permission_classes=[AllowAny])
+    def register(self,request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    @action(detail=False, methods=['get'], permission_classes=[AllowAny])
+    def login(self,request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user:
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key}, status=status.HTTP_200_OK)
+        return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+
+
+    @action(detail=False, methods=['get'], permission_classes=[IsAuthenticated])
+    def profile(self, request):
+        serializer = self.get_serializer(request.user)
+        return Response(serializer.data)
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
@@ -43,7 +66,7 @@ class TaskViewSet(viewsets.ModelViewSet):
         # Set the owner of the task to the logged-in user
         serializer.save(owner=self.request.user)
     
-    @action(detail= True, methods=["post"])
+    @action(detail= True, methods=["post"], )
     def mark_complete(self, request, pk=None):
         task = self.get_object()
         task.mark_completed()
@@ -56,3 +79,13 @@ class TaskViewSet(viewsets.ModelViewSet):
         task.mark_incomplete()
         serializer = self.get_serializer(task)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @action(detail=True, methods=['patch'], permission_classes=[IsAuthenticated, IsOwner])
+    def complete(self, request, pk=None):
+        task = self.get_object()
+        if task.status == 'completed':
+            task.mark_incomplete()
+        else:
+            task.mark_completed()
+        serializer = self.get_serializer(task)
+        return Response(serializer.data)
